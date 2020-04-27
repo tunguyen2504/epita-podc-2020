0e6cda7d63fd81cccdd068dcae057ee37d5f0419
Nguyễn Anh Tú <tunguyen.hust@gmail.com>
2020-04-27 16:45:37 +0200

    Update combine function for portrait

--- painting_spliter.py
+++ painting_spliter.py
@@ -1,41 +1,51 @@
 
 # coding: utf-8
 
-# In[121]:
+# In[102]:
 
 
 class Reader:
     def __init__(self, input_file):
+        self.portrait_list = '';
+        self.landscape_list = '';
+        self.new_text = '';
         file = open(input_file, "r");
         lines = file.readlines()[1:];
         i = 0;
-        new_text = '';
         for line in lines:
-            new_text += str(i) + ' ' + line;
+            self.new_text += str(i) + ' ' + line;
             i += 1;
         file.close()
-        portrait_list = '';
-        landscape_list = '';
-        for line in new_text.splitlines():
+        
+    def paint_split(self):
+        for line in self.new_text.splitlines():
             pType = line.split(' ')[1];
             if (pType == 'P'):
-                portrait_list += line + '\n';
+                self.portrait_list += line + '\n';
             elif (pType == 'L'):
-                landscape_list += line + '\n';
-        full_list = landscape_list + portrait_list;
-        print(full_list);
-#         output_file = input_file + "_reorder.txt";
-#         new_file = open(output_file, "w");
-#         new_file.write(full_list);
-#         new_file.close();
-#         print(landscape)
-#         print(outtext)
-# outfile = open("3_2000.txt","w")
-# outfile.writelines(outtext)
-# outfile.close()
+                self.landscape_list += line + '\n';
+        result = self.landscape_list + self.portrait_list;
+        return result;
+    
+    def portrait_combiner(self):
+        p_list = self.portrait_list.splitlines();
+        print(int(len(p_list)/2));
+        result = '';
+        for i in range(0, int(len(p_list)/2)):
+            p1_id = p_list[i].split(' ')[0];
+            p2_id = p_list[len(p_list)-1-i].split(' ')[0];
+            p1_tags = p_list[i].split(' ')[3:];
+            p2_tags = p_list[len(p_list)-1-i].split(' ')[3:];
+            tag_list = list(set(p1_tags) | set(p2_tags));
+            result += str(p1_id) + ',' + str(p2_id) + ' ';
+            for tag in tag_list:
+                if (tag == tag_list[-1]):
+                    result += tag + '\n'
+                else: result += tag + ' ';
+        print(result);
 
 
-# In[122]:
+# In[103]:
 
 
 import argparse
@@ -45,10 +55,15 @@ def main():
 
     args = parser.parse_args()
 
-    read = Reader(args.input)
+    reader = Reader(args.input)
+    reader.paint_split();
+    reader.portrait_combiner();
     
 if __name__ == "__main__":
     main()
     
-# read = Reader("../10_computable_moments.txt")
+# reader = Reader("../10_computable_moments.txt");
+# reader.paint_split();
+# print(reader.portrait_list);
+# reader.portrait_combiner();
 
