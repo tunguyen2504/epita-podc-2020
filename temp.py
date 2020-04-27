f6ed9eeb54cb765bec31128f989fd3324f84e10a
j0sephmerheb <joseph@merheb.net>
2020-04-27 13:31:33 +0200

    testing the comparison fnc

--- temp.py
+++ temp.py
@@ -1,17 +1,29 @@
-pa1 = ['sea', 'blue', 'sky', 'water']
-pa2 = ['python', 'blue', 'sky']
+# Static list for testing
+fg1 = ['sea', 'blue', 'sky', 'water']
+fg2 = ['python', 'blue', 'sky']
 
-# Just an example
-def compare_paintings(pa1, pa2):
-    len1 = len(pa1)
-    len2 = len(pa2)
-    intOfList = list(set(pa1) & set(pa2))
+# Compare FrameGlasses
+def compare_paintings(fg1, fg2):
+    # Number of tags common to fg1 and fg2
+    intOfList = list(set(fg1) & set(fg2))
     len3 = len(intOfList)
 
+    # Number of tags in fg1 but not in fg2
+    len1 = len(fg1) - len3
+
+    # Number of tags in fg2 but not in fg1
+    len2 = len(fg2) - len3
+    
+    # Temporary List
+    tmpList = []
     tmpList.append(len1)
     tmpList.append(len2)
     tmpList.append(len3)
     tmpList.sort()
-    return min(tmpList)
 
-compare_paintings(pa1, pa2)
+    # Return the local satisfaction score
+    #return(min(tmpList))
+    print(min(tmpList))
+
+# To be removed later
+compare_paintings(fg1, fg2)
